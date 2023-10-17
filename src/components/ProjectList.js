78b59adfa8c80737ad3619310ddf586514038ac8
Alex <alextrotman04@gmail.com>
2023-10-17 18:08:00 -0500

    finish

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,21 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectListObjects = projects.map((obj) => {
+    return (
+      <ProjectItem
+        key={obj.name}
+        name={obj.name}
+        technologies={obj.technologies}
+        about={obj.about}
+      />
+    );
+  });
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectListObjects}</div>
     </div>
   );
 }
